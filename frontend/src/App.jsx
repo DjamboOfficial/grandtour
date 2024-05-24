18b93dc8dbe822903bc3cb217055a22f3b34cf22
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2024-05-24 11:12:32 +0200

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,14 @@ import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
+import Header from "./components/Header";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/sign-in" element={<SignIn />}></Route>
