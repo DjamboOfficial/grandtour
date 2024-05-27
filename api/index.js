01494aed03901b8f312cf7daba400ab616f31ec3
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2024-05-27 11:02:56 +0200

    ciao

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Mongo is connected!");
+  })
+  .catch((error) => {
+    console.log(err);
+  });
 
 const app = express();
 
