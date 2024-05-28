8437b378a29cfc6c2ff10daa4a9b7f17abb88a0b
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2024-05-28 10:57:11 +0200

    sign-up api interface & route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,15 @@
-import User from "../models/User.js";
-import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import User from "../models/User.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json({ message: "User created successfully" });
+    res.status(201).json("User created successfully!");
   } catch (error) {
-    next(errorHandler(550, "Error from the errorHandler function!"));
+    next(error);
   }
 };
