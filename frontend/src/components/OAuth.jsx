a51051e3c7b75bb3582abed75bb2c7d856e86d04
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2024-05-29 16:36:21 +0200

    google auth

--- frontend/src/components/OAuth.jsx
+++ frontend/src/components/OAuth.jsx
@@ -1,16 +1,17 @@
-import React from "react";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
-import { signInSuccess } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
-    const dispatch = useDispatch();
-
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
+
       const result = await signInWithPopup(auth, provider);
 
       const res = await fetch("/api/auth/google", {
@@ -24,19 +25,18 @@ export default function OAuth() {
           photo: result.user.photoURL,
         }),
       });
-
       const data = await res.json();
       dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
-      console.log("Could not sign in with Google: ", error);
+      console.log("could not sign in with google", error);
     }
   };
-
   return (
     <button
       onClick={handleGoogleClick}
       type="button"
-      className="bg-red-700 text-white p-3 rouded-lg uppercase hover:opacity-95 disabled:opacity-80"
+      className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
     >
       Continue with google
     </button>
