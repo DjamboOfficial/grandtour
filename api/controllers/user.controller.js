c296be97f01fe3398d7c742c8290dfed456a15c1
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2024-05-31 10:36:41 +0200

    create user update API

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,33 @@
+import User from "../models/User";
+import { errorHandler } from "../utils/error";
+import bcryptjs from "bcryptjs";
+
 export const test = (req, res) => {
   res.json({ message: "Hello World!" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "Not authenticated!"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            avatar: req.body.avatar,
+          },
+        },
+        { new: true }
+      );
+    }
+    const { password: pass, ...rest } = updatedUser._doc;
+    res.status(200, "All good!");
+  } catch (error) {
+    next(error);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 mongoose
@@ -13,8 +14,11 @@ mongoose
   .catch((error) => {
     console.log(err);
   });
+
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
