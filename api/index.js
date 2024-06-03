833ca476ee2df08b35f8aaab2e43938c0ef17a8f
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2024-06-03 10:28:22 +0200

    create listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listing.route.js";
 
 dotenv.config();
 mongoose
@@ -22,6 +23,7 @@ app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000!");
